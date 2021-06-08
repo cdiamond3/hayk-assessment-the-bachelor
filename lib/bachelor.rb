b7642cd79869fedb92b15d53a8c77741e71a0f51
Chris Diamond <diamond.chris.3@gmail.com>
2021-06-08 17:18:45 -0600

    with comments

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,22 +1,22 @@
-def get_first_name_of_season_winner(data, season)
-  winner = data[season].find do |contestant|
-    contestant["status"] == "Winner"
+def get_first_name_of_season_winner(data, season) #this is our method that is taking in 2 arguements, the hash and the value we are looking for
+  winner = data[season].find do |contestant| #we set a variable to equal data(which is the entire array)and the value of the passed in season and we are telling it to find every contestant
+    contestant["status"] == "Winner" #for every contestant found compare to see if the status is equal to "Winner"
   end
-    winner["name"].split[0]
+    winner["name"].split[0] #return our variable with .split and index it at 0 to split the first name
 end
 
 def get_contestant_name(data, occupation)
-  matching_contestant = data.values.flatten.find do |contestant|
-    contestant["occupation"] == occupation
+  matching_contestant = data.values.flatten.find do |contestant| #using .flatten will take away one layer of the array
+    contestant["occupation"] == occupation #we are checking to see if the occupation of the contestant matches our passed in occupation
   end
-  matching_contestant["name"]  
+  matching_contestant["name"]  #returning the contestant with the right occupation's name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  data.values.flatten.reduce(0) do |count, contestant|
-    if contestant["hometown"] == hometown
+  data.values.flatten.reduce(0) do |count, contestant| #reduce will start a counter
+    if contestant["hometown"] == hometown #this statement is saying that every contestant that has a hometown that matches the one that we passed in add 1 to the count.
       count + 1
-    else
+    else #if not return the count
       count
     end
   end
